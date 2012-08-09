e9f9335af4b611574056c9948c0a098b4ba31379
johnjbarton <johnjbarton@johnjbarton.com>
2012-08-08 22:02:13 -0700

    Complete rewrite; Works in devtools on devtools, reloads the test-victim and adds a iframe to it containing the LayoutTests file; Also can get the LayoutTests URL from the web page that the test-victim is debugging

--- testDevtools.js
+++ testDevtools.js
@@ -2,77 +2,9 @@
 // Copyright 2011 Google Inc. johnjbarton@google.com
 console.log("testDevtools begins %o", chrome);
 
-var reloadedForTesting = false;
-
-function loadTestRunnerScript() {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "../testRunner.js", false);
-  xhr.send(null);
-  return  xhr.responseText;
-}
-
-var testRunnerScript = loadTestRunnerScript();
-
-function reloadForTesting() {
-  console.log("injectedScript: ", testRunnerScript);
-  reloadedForTesting = true;
-  var clearAll = "if (window.InspectorTest) window.InspectorTest.removeSniffers();\n"
-  clearAll += "delete window.InspectorTest;\n";
-  clearAll += "delete window.results";
-  var forWebInspector = {
-      method: "evaluateInWebInspector",
-      args: [clearAll, "clearAll.js"]
-  };
-  window.parent.postMessage(forWebInspector, "*")
-  chrome.devtools.inspectedWindow.reload({injectedScript: testRunnerScript});
-}
-
-function beginTesting() {
-  chrome.devtools.inspectedWindow.eval(
-    "window.testRunner.forWebInspector", 
-    function(forWebInspector) {
-      console.log("beginTesting evaled forWebInspector "+forWebInspector.method, forWebInspector);
-      if (forWebInspector) 
-        window.parent.postMessage(forWebInspector, "*");
-    });
-}
-
-function onNavigated(url){
-  console.log("onNavigated "+url+' reloadedForTesting '+reloadedForTesting);
-  if (reloadedForTesting) {
-    reloadedForTesting = false;
-    beginTesting();
-  } else {
-    reloadForTesting();
-  }
-}
-
-function lookForEvaluateEvent(message) {
-  console.log("lookForEvaluateEvent "+message.text);
-  if (message.text .indexOf('testRunnerMessage:')=== 0) {
-    beginTesting();
-  }
-}
-
-function enableTesting() {
-  //chrome.devtools.network.onNavigated.addListener(onNavigated);
-  chrome.experimental.devtools.console.onMessageAdded.addListener(lookForEvaluateEvent);
-}
-
-chrome.devtools.inspectedWindow.eval(
-  "window.location.href",
-  function onEval(url) {
-    console.log("url "+url);
-    if (url.indexOf('/inspector/') !== -1) {  // We are the victim of testing
-      enableTesting();
-      chrome.devtools.panels.create("testRunner", "testRunnerIcon.png", "testRunnerPanel.html", function(panel) {
-          panel.onShown.addListener(function (panel_window) {
-            panel_window.addEventListener('message', function(event) {
-              reloadForTesting();
-            });
-          });
-      });
-    }
-  }
-);
+chrome.devtools.panels.create("testRunner", "testRunnerIcon.png", "testRunnerPanel.html", function(panel) {
+  panel.onShown.addListener(function (panel_window) {
+    console.log("testRunnerPanel onShown");
+  });
+});
 
--- testRunnerPanel.js
+++ testRunnerPanel.js
@@ -1,12 +1,58 @@
 // Google BSD license http://code.google.com/google_bsd_license.html
 // Copyright 2011 Google Inc. johnjbarton@google.com
 
-function reloadForTesting() {
-  window.postMessage("reloadForTesting", "*");
+function setTestCaseURL() {
+  // Put the URL into the UI
+  function onURL(url) {
+    var testCaseURL = document.querySelector(".testCaseURL");
+    testCaseURL.value = url;
+  }
+  // Reach into the devtools web app can find out what window it is inspecting
+  chrome.devtools.inspectedWindow.eval("WebInspector.inspectedPageURL", onURL);
+}
+
+function getTestCaseURL() {
+  return document.querySelector(".testCaseURL").value;
+}
+
+function getTestingScript(testCaseURL) {
+
+  // This function will be sent as source into the to-be-tested-devtools
+  function addFrameBeforeLoad(testCaseURL) {
+    console.log("addFrameBeforeLoad "+testCaseURL);
+    function onDOMContentLoaded() {
+      // inspectedWindow.eval() operates on the iframe we add, so don't recurse
+      if (document.body.id === "-webkit-web-inspector") {
+        console.log("onDOMContentLoaded into "+window.location+" with id "+document.body.id+" for test "+testCaseURL);
+        var iframe = document.createElement('iframe');
+        iframe.src = testCaseURL;
+        iframe.classList.add("testRunner");
+        document.body.appendChild(iframe);
+        window.removeEventListener('DOMContentLoaded', onDOMContentLoaded);        
+      }
+    }
+
+    window.addEventListener('DOMContentLoaded', onDOMContentLoaded);
+  }
+
+  return "(" + addFrameBeforeLoad + ")(\"" + testCaseURL + "\");";
+}
+
+function reloadForTesting(testCaseURL) {
+  var injectedTestingScript = getTestingScript(testCaseURL);
+  console.log("injectedScript: ", injectedTestingScript);
+  chrome.devtools.inspectedWindow.reload({injectedScript: injectedTestingScript});
+}
+
+function runTest() {
+  var url = getTestCaseURL();
+  if (url) {
+    reloadForTesting(url);
+  }
 }
 
 window.addEventListener('load', function() {
-  var runTestElt = document.querySelector('.runTest');
-  runTestElt.addEventListener('click', reloadForTesting);
+  document.querySelector('.runTest').addEventListener('click', runTest);   
+  document.querySelector('.setTestCaseURL').addEventListener('click', setTestCaseURL);   
 });
 
