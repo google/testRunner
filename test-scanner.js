d21cb19acfba6ba95e37bdbfa9e792ad5974be22
johnjbarton <johnjbarton@johnjbarton.com>
2013-01-03 09:06:20 -0800

    WIP CSP workaround

--- test-scanner.js
+++ test-scanner.js
@@ -1,3 +1,4 @@
+
 var tests = [];
 var baseURL = "http://localhost:9696"
 scanFolder("inspector/console");
@@ -9,28 +10,52 @@ scanFolder("inspector/styles");
 scanFolder("inspector/timeline");
 scanFolder("inspector");
 
+function request(method, url, callback, errback) {
+    if (!this.requestCreator) {
+        this.requestCreator = new ChannelPlate.RequestCreator(ChannelPlate.DevtoolsTalker);
+    }
+    this.requestCreator.request('PROPFIND', [url], function() {
+        if (arguments[0] === "Error") {
+          var message = arguments[1];
+          console.error("XHR Failed for "+url, message);
+          errback(message);
+        } else {
+          callback(arguments[0]);
+        }
+  });
+}
+/*
+function request(method, url, callback, errback) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = callback;
+    xhr.onerror = errback;
+    xhr.open(method, url);
+    if (method === "PROFFIND") 
+      xhr.responseType = "document";
+    xhr.send(null);  
+}
+*/
+
 function scanFolder(folder)
 {
-    var xhr = new XMLHttpRequest();
-    xhr.onload = function() {
-        console.log(xhr.responseXML.documentElement.nodeName);
-        var doc = xhr.responseXML;
-        var links = doc.querySelectorAll("a");
-        for (var i = 0; i < links.length; ++i) {
-            var link = links[i].href;
-            var match = link.match(/[^\/]*\/([^\/]+\.html)$/);
-            if (!match)
-                continue;
-            var path = "/LayoutTests/" + folder + "/" + match[1];
-            fetchExpectations(link);
+    var url = baseURL+"/LayoutTests/" + folder + "/";
+    request('PROPFIND', url, function onload() {
+            console.log(xhr.responseXML.documentElement.nodeName);
+            var doc = xhr.responseXML;
+            var links = doc.querySelectorAll("a");
+            for (var i = 0; i < links.length; ++i) {
+                var link = links[i].href;
+                var match = link.match(/[^\/]*\/([^\/]+\.html)$/);
+                if (!match)
+                    continue;
+                var path = "/LayoutTests/" + folder + "/" + match[1];
+                fetchExpectations(link);
+            }
+        },
+        function onerror(message) {
+          console.error(window.location + ": XHR failed ", message);
         }
-    }
-    xhr.onerror = function(message) {
-      console.error(window.location + ": XHR failed ", message);
-    }
-    xhr.open("GET", baseURL+"/LayoutTests/" + folder + "/");
-    xhr.responseType = "document";
-    xhr.send(null);
+    );
 }
 
 function fetchExpectations(path, callback)
