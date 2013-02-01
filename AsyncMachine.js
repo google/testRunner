d00df1291811f086ba602d6a963b97f9a3233ed1
johnjbarton <johnjbarton@johnjbarton.com>
2013-01-31 16:28:18 -0800

    allow the op to process the result value

--- AsyncMachine.js
+++ AsyncMachine.js
@@ -8,15 +8,23 @@ var AsyncMachine = {
   pushOp: function(fnc, opt_args) {
     var args = Array.prototype.slice.apply(arguments);
     args.shift();
+    var resultProcessor;
+    if (typeof args[args.length - 1] === 'function') {
+        resultProcessor = args.pop();
+      }
     args.push(this.runOp.bind(this));
-    this.ops.push({fnc: fnc, args: args});
+    this.ops.push({fnc: fnc, args: args, resultProcessor: resultProcessor});
   },
   runOp: function(prevResult) {
     if (prevResult) {
+      if (this.resultProcessor) {
+        prevResult = this.resultProcessor(prevResult);
+      }
       console.log("InspectorTest.addResult: " + prevResult);
     }
     var op = this.ops.shift();
     if (op) {
+      this.resultProcessor = op.resultProcessor;
       var fncName = op.fnc.toString().match(/function.*?{/);
       console.log("Running test operation " + fncName, op);
       op.fnc.apply(this, op.args);
